ee4cde33d59ec5029c9efe9f249c24a3edac9629
JP <sr.joaovirgili@gmail.com>
2020-09-23 21:30:38 -0300

    chatzinho

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
+
+io.on('connection', (client) => {
+    console.log(client.id + ' connected');
+
+    client.on('chat message', (msg) => {
+        console.log(msg);
+        io.emit('messages', client.id + ": " + msg);
+    });
+
+    client.on('disconnect', () => {
+        console.log(client.id + ' disconnected');
+    });
+});
+
+
 
 http.listen(3000, () => {
     console.log('listening on *:3000');

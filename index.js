8722daef7c79daaa7d875e4648f2411b8ba5e0bd
JP <sr.joaovirgili@gmail.com>
2020-09-24 23:55:09 -0300

    Ajustado a porta

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+const PORT = process.env.PORT || 3000;
+
 io.on('connection', (client) => {
     console.log(client.id + ' connected');
 
@@ -21,7 +23,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+http.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
 });

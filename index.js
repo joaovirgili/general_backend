4b70195c6542260554f8dde9a8cea20102b3c3ad
JP <sr.joaovirgili@gmail.com>
2020-09-24 23:40:17 -0300

    Add cool face API

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var app = require('express')();
+
+const express = require('express');
+const app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-});
+const cool = require('cool-ascii-faces');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 io.on('connection', (client) => {
     console.log(client.id + ' connected');
@@ -19,8 +21,22 @@ io.on('connection', (client) => {
     });
 });
 
-
-
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 });
+
+app
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/cool', (req, res) => res.send(cool()))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+
+
+
+
+// http.listen(3000, () => {
+//     console.log('listening on *:3000');
+// });

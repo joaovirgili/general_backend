12619c957df2abaf474254713ae546f9d736fa43
JP <sr.joaovirgili@gmail.com>
2020-09-24 23:51:33 -0300

    FOI

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-const cool = require('cool-ascii-faces');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-
 io.on('connection', (client) => {
     console.log(client.id + ' connected');
 
@@ -25,18 +21,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/cool', (req, res) => res.send(cool()))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-
-
-
-
-
-// http.listen(3000, () => {
-//     console.log('listening on *:3000');
-// });
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});

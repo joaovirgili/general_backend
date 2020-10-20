a25918b53cf085526591b1ce504fdf745a469ea2
pedrojbeckhauser <pedrojbeckhauser@gmail.com>
2020-10-20 20:16:02 -0300

    bla

--- index.js
+++ index.js
@@ -3,24 +3,23 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (client) => {
-    console.log(client.id + ' connected');
+	console.log(client.id + ' connected');
 
-    client.on('chat message', (msg) => {
-        console.log(msg);
-        io.emit('messages', client.id + ": " + msg);
-    });
+	client.on('send message', (msg) => {
+		console.log(msg);
+		// client.broadcast.emit('messages', client.id + ': ' + msg);
+		io.emit('messages', `${client.id}: ${msg}`);
+	});
 
-    client.on('disconnect', () => {
-        console.log(client.id + ' disconnected');
-    });
+	client.on('disconnect', () => {
+		console.log(client.id + ' disconnected');
+	});
 });
 
-
-
 http.listen(3000, () => {
-    console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
